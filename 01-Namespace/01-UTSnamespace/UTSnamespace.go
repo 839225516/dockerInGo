76ec1c80a89989b7ce7a50c7e1b4d04daca95e50
zengxianglin <839225516@qq.com>
2019-04-05 01:39:56 +0800

    UST namespace 隔离nodename 和 domainname， eg: hostname

--- 01-Namespace/01-UTSnamespace/UTSnamespace.go
+++ 01-Namespace/01-UTSnamespace/UTSnamespace.go
@@ -30,3 +30,26 @@ func main() {
 	}
 
 }
+
+/*
+	在linux上执行go run UTSnamespace.go
+
+	// 输出一个当前的PID
+	# echo $$
+	3054
+
+	// 在另一个shell里用 pstree -pl 查看进程关系
+           ├─sshd(1226)───sshd(2995)─┬─bash(2997)───go(3034)─┬─UTSnamespace(3051)─┬─sh(3054)
+           │                         │                       │                    ├─{UTSnamespace}(3052)
+           │                         │                       │                    └─{UTSnamespace}(3053)
+           │                         │                       ├─{go}(3035)
+           │                         │                       ├─{go}(3036)
+           │                         │                       ├─{go}(3038)
+           │                         │                       └─{go}(3039)
+           │                         └─bash(3063)───pstree(3080)
+
+	// 由于UST namespace 对 hostname做了隔离，所有在这个环境内修改不影响外部主机
+	# hostname -b dockertest
+	# hostname
+	dockertest
+*/
