64b88e556df78f931386aa89fba96dff815596e7
zengxianglin <839225516@qq.com>
2019-04-07 13:30:13 +0800

    PID namespace 用来隔离进程ID

---
+++ 01-Namespace/03-PIDnamespace/PIDnamespace.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.SLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID,
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
